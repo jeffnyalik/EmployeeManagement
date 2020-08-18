fecf37ed2352ce87f83b539b608a3f1fa3b24ef6
Jeff Nyalik <jeffnyak@gmail.com>
2020-08-18 16:20:13 +0300

    Adding and Editing roles

--- Controllers/AdminstrationController.cs
+++ Controllers/AdminstrationController.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagement.Models;
 using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace EmployeeManagement.Controllers
 {
     public class AdminstrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        public AdminstrationController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AdminstrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
            _roleManager = roleManager;
+            _userManager = userManager;
         }
        
         [HttpGet]
@@ -52,6 +57,57 @@ namespace EmployeeManagement.Controllers
         {
             var roles = _roleManager.Roles;
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {role} does not exist";
+                return View("NotFoundCode");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                Role = role.Name,
+            };
+
+            foreach(var user in _userManager.Users)
+            {
+                if(await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {role} does not exist";
+                return View("NotFoundCode");
+            }
+            else
+            {
+                role.Name = model.Role;
+               var result =  await _roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
         }
     }
 }
